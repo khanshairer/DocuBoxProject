dc7744c859fa6453c8a78e64ef43f552d20cc3cc
Alen Jacob <ajseby@mun.ca>
2025-06-10 17:15:01 -0230

    Fix Flutter analyze warnings - remove unused imports

--- lib/screens/upload_page_ajseby/document_upload_page.dart
+++ lib/screens/upload_page_ajseby/document_upload_page.dart
@@ -3,7 +3,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 
 class DocumentUploadPage extends StatefulWidget {
