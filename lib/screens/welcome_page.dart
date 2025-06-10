b5c48e4d2ded6621057da7b5f4eb05d3a3485378
Sharier Khan <mrkhansharier@gmail.com>
2025-06-10 15:02:07 -0230

    add googel_fonts package for designing welcome page

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart'; // Import your login/signup page
+import 'package:google_fonts/google_fonts.dart';
+
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
