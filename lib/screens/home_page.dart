a15120f660f2f2a59184fb87afef5ded56fce766
Sharier Khan <mrkhansharier@gmail.com>
2025-06-10 22:32:06 -0230

    add missign curly bracket

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,17 +6,17 @@ import '../providers/auth_state_provider.dart';
 import '../widget/homepage_menu_bar_widget.dart';
 import 'package:go_router/go_router.dart';
 
-
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
-  
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // Watch the authStateProvider to get the FirebaseAuthStateNotifier instance.
     // This widget will rebuild whenever authNotifier.notifyListeners() is called.
     final authNotifier = ref.watch(authStateProvider);
-    final user = authNotifier.currentUser; // Directly access the current user from the notifier
+    final user =
+        authNotifier
+            .currentUser; // Directly access the current user from the notifier
 
     // Since GoRouter's redirect logic handles sending unauthenticated users
     // to the login page, this HomePage should only build when a user is logged in.
@@ -26,11 +26,7 @@ class HomePage extends ConsumerWidget {
       // This case should ideally be handled by GoRouter's redirect,
       // but a minimal empty container can act as a temporary placeholder
       // while the navigation system redirects the user.
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
     return Scaffold(
@@ -40,9 +36,12 @@ class HomePage extends ConsumerWidget {
         foregroundColor: Colors.white,
         actions: [
           // upload Document
-          IconButton(onPressed: (){
-            context.go('/document-upload');
-          }, icon: Icon(Icons.upload)),
+          IconButton(
+            onPressed: () {
+              context.go('/document-upload');
+            },
+            icon: Icon(Icons.upload),
+          ),
           // add an inconbutton with notification icon
           IconButton(onPressed: () {}, icon: Icon(Icons.notifications_active)),
         ],
@@ -97,14 +96,12 @@ class HomePage extends ConsumerWidget {
                   ],
                 ),
               ),
-              
+
               const SizedBox(height: 40),
-              
-                          ],
+            ],
           ),
         ),
       ),
     );
   }
-
-  
+}
